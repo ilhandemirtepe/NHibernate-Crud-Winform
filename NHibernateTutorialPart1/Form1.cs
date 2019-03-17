b2a2942c4e901c05a51204081a9b5a70122cc0c2
ilhan <ilhandemirtepe@gmail.com>
2019-03-17 19:06:55 +0300

    Nhibernate crud
    Nhibernate crud

---
+++ NHibernateTutorialPart1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using NHibernateCRUD;
+
+namespace NHibernateCRUD
+{
+    public partial class Form1 : Form
+    {
+        private Configuration myConfiguration;
+        private ISessionFactory mySessionFactory;
+        private ISession mySession;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            myConfiguration = new Configuration();
+            myConfiguration.Configure();
+            mySessionFactory = myConfiguration.BuildSessionFactory();
+            mySession = mySessionFactory.OpenSession();
+
+            // Add Record
+            using (mySession.BeginTransaction())
+            {
+                Contact loContact = new Contact { FirstName = "Ali", LastName = "Kaya" };
+                mySession.Save(loContact);
+              
+
+                mySession.Transaction.Commit();
+            }
+
+            // List Contact
+
+            using (mySession.BeginTransaction())
+            {
+                ICriteria criteria = mySession.CreateCriteria<Contact>();
+                IList<Contact> list = criteria.List<Contact>();
+
+                mySession.Transaction.Commit();
+            }
+
+
+
+            // Delete Contact
+
+            using (mySession.BeginTransaction())
+            {
+
+                var item= mySession.Get<Contact>(2);
+                mySession.Delete(item);
+
+                mySession.Transaction.Commit();
+            }
+
+        }
+    }
+}
